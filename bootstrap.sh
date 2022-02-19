670cd4a1cbd724f37341259b538bce17c50d2e94
Evan Sosenko <razorx@evansosenko.com>
2022-02-19 00:32:55 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,12 @@ set -u
 
 cp -as "$(pwd)/.zshenv" $HOME
 cp -as "$(pwd)/.config/" $HOME
+
+sudo apt-get install -y \
+  ack \
+  httpie \
+  ripgrep \
+  silversearcher-ag
+
+curl -L https://git.io/v5XSs | sh
+ZPLUG_PROTOCOL=HTTPS zsh
